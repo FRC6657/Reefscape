51124f5d379cc2fc35a006e9828f9db77c020d3c
IShaLinI <andrewccard04@gmail.com>
2025-03-01 08:47:46 -0800

    new encoder settings

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 175;
-  public static final String GIT_SHA = "88db33ceafa960f3c74948ba916d54ddea3f62c1";
-  public static final String GIT_DATE = "2025-03-01 10:27:20 EST";
+  public static final int GIT_REVISION = 176;
+  public static final String GIT_SHA = "c921333213b66a3700402d56ce228e101e0e8523";
+  public static final String GIT_DATE = "2025-03-01 11:36:23 EST";
   public static final String GIT_BRANCH = "PortH-Testing";
-  public static final String BUILD_DATE = "2025-03-01 11:35:50 EST";
-  public static final long BUILD_UNIX_TIME = 1740846950671L;
+  public static final String BUILD_DATE = "2025-03-01 11:46:27 EST";
+  public static final long BUILD_UNIX_TIME = 1740847587855L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
