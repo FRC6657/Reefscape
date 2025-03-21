811baa696770e12c777d7a4f99c45b8d5beb56da
Andrew Card <andrewccard04@gmail.com>
2025-03-21 16:45:01 -0700

    rps

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 248;
-  public static final String GIT_SHA = "b5be350455f99aa28b40d96fad3535802b7e82e8";
-  public static final String GIT_DATE = "2025-03-21 18:50:42 EDT";
+  public static final int GIT_REVISION = 251;
+  public static final String GIT_SHA = "ffdc885fa9577a9b6087ab3cce3b45084cec814a";
+  public static final String GIT_DATE = "2025-03-21 19:39:20 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-21 18:53:41 EDT";
-  public static final long BUILD_UNIX_TIME = 1742597621307L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2025-03-21 19:44:53 EDT";
+  public static final long BUILD_UNIX_TIME = 1742600693126L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
--- src/main/java/frc/robot/subsystems/de_algaefier/De_algaefierIO_Real.java
+++ src/main/java/frc/robot/subsystems/de_algaefier/De_algaefierIO_Real.java
@@ -7,8 +7,6 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.math.util.Units;
@@ -27,7 +25,6 @@ public class De_algaefierIO_Real implements De_algaefierIO {
   private ProfiledPIDController pivotPID =
       new ProfiledPIDController(
           4, 0, 0, new Constraints(Units.degreesToRadians(200), Units.degreesToRadians(200)));
-
 
   public De_algaefierIO_Real() {
 
@@ -53,7 +50,7 @@ public class De_algaefierIO_Real implements De_algaefierIO {
     inputs.kSetpoint = kSetpoint;
 
     inputs.kPosition = Units.rotationsToDegrees(kEncoder.getPosition());
-    inputs.kVelocity = Units.rotationsToDegrees(kEncoder.getVelocity());
+    inputs.kVelocity = Units.rotationsToDegrees(kEncoder.getVelocity() / 60d);
 
     inputs.kCurrent = kPivot.getOutputCurrent();
     inputs.kVoltage = kPivot.getAppliedOutput() * RobotController.getBatteryVoltage();
@@ -63,8 +60,6 @@ public class De_algaefierIO_Real implements De_algaefierIO {
 
     double pidOutput = pivotPID.calculate(inputs.kPosition, kSetpoint);
     kPivot.setVoltage(pidOutput);
-
-    
   }
 
   @Override
