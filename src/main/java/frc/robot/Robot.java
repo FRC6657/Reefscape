bfad784fad1b44b6bcff92e31a12ff24a26a0fa8
Andrew Card <andrewccard04@gmail.com>
2025-03-21 16:02:12 -0700

    stop rumble properly

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -186,7 +186,7 @@ public class Robot extends LoggedRobot {
                                 -MathUtil.applyDeadband(driver.getRightX(), 0.1)
                                     * Constants.Swerve.maxAngularSpeed
                                     * 0.1)))));
-    driver.a().onFalse(Commands.runOnce(() -> swerve.drive(new ChassisSpeeds())));
+    driver.a().onFalse(Commands.runOnce(() -> swerve.drive(new ChassisSpeeds())).andThen(rumble(0, 0)));
 
     operator.button(9).onTrue(superstructure.selectElevatorHeight(2));
     operator.button(8).onTrue(superstructure.selectElevatorHeight(3));
