3bd829e93313ab9e86ca1750731c8a3b91b09ed7
Andrew Card <andrewccard04@gmail.com>
2025-03-22 21:17:45 -0700

    add profiler lines.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 277;
-  public static final String GIT_SHA = "482caa887c1230976f6bcdd41ebebc0d1ce0a47e";
-  public static final String GIT_DATE = "2025-03-22 15:24:24 EDT";
-  public static final String GIT_BRANCH = "ClimberReenable";
-  public static final String BUILD_DATE = "2025-03-22 15:53:56 EDT";
-  public static final long BUILD_UNIX_TIME = 1742673236844L;
+  public static final int GIT_REVISION = 278;
+  public static final String GIT_SHA = "434882b48a6a549ab92bf4b6e5fa532e2b8792f6";
+  public static final String GIT_DATE = "2025-03-22 15:59:27 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-03-23 00:13:57 EDT";
+  public static final long BUILD_UNIX_TIME = 1742703237869L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
