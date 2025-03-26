8b0d9a830f5048abf7c895084a498a900d744af8
Andrew-6657 <andrew.frankovic@uphsconnect.org>
2025-03-26 16:34:47 -0700

    final adjustment, speed slowed

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 284;
-  public static final String GIT_SHA = "fb24abff2ba2e67755dbd57d37a476573bdbc23a";
-  public static final String GIT_DATE = "2025-03-26 19:21:11 EDT";
+  public static final int GIT_REVISION = 285;
+  public static final String GIT_SHA = "8b78d1763a64914c6943c7c9f99f0bdb437a144d";
+  public static final String GIT_DATE = "2025-03-26 19:29:24 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-26 19:28:34 EDT";
-  public static final long BUILD_UNIX_TIME = 1743031714048L;
+  public static final String BUILD_DATE = "2025-03-26 19:32:19 EDT";
+  public static final long BUILD_UNIX_TIME = 1743031939209L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
