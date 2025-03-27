9ac2f9f8bbc3ac42335d743b62c690217910fe75
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2025-03-26 17:16:31 -0700

    slight config change

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,14 +3,14 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Reefscape";
+  public static final String MAVEN_NAME = "Main Branch";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 286;
-  public static final String GIT_SHA = "8b0d9a830f5048abf7c895084a498a900d744af8";
-  public static final String GIT_DATE = "2025-03-26 19:34:47 EDT";
+  public static final int GIT_REVISION = 287;
+  public static final String GIT_SHA = "4e2b5a358e9b6e311fa685320c22268bf3cc78ee";
+  public static final String GIT_DATE = "2025-03-26 20:14:02 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-26 20:12:34 EDT";
-  public static final long BUILD_UNIX_TIME = 1743034354274L;
+  public static final String BUILD_DATE = "2025-03-26 20:15:57 EDT";
+  public static final long BUILD_UNIX_TIME = 1743034557108L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
