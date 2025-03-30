a485cec37e78647a5146ccfd20aa8709b42babf0
Andrew Card <andrewccard04@gmail.com>
2025-03-30 10:29:43 -0700

    Update Photonvision

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 309;
-  public static final String GIT_SHA = "3eed304c26acd25a8fb357e800303144ed0089b3";
-  public static final String GIT_DATE = "2025-03-29 16:10:22 EDT";
-  public static final String GIT_BRANCH = "Second-Sanger";
-  public static final String BUILD_DATE = "2025-03-29 17:05:47 EDT";
-  public static final long BUILD_UNIX_TIME = 1743282347412L;
+  public static final int GIT_REVISION = 311;
+  public static final String GIT_SHA = "e9236131ea3578555a95c442658d54b55d998c56";
+  public static final String GIT_DATE = "2025-03-30 13:19:28 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-03-30 13:29:06 EDT";
+  public static final long BUILD_UNIX_TIME = 1743355746499L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
