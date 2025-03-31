89758dfd6ade92c9c5a6ef18d7a60fca6694a597
Andrew Card <andrewccard04@gmail.com>
2025-03-31 08:32:53 -0700

    somewhat fixed jitter

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 311;
-  public static final String GIT_SHA = "e9236131ea3578555a95c442658d54b55d998c56";
-  public static final String GIT_DATE = "2025-03-30 13:19:28 EDT";
+  public static final int GIT_REVISION = 312;
+  public static final String GIT_SHA = "a485cec37e78647a5146ccfd20aa8709b42babf0";
+  public static final String GIT_DATE = "2025-03-30 13:29:43 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-30 13:29:06 EDT";
-  public static final long BUILD_UNIX_TIME = 1743355746499L;
+  public static final String BUILD_DATE = "2025-03-31 11:13:17 EDT";
+  public static final long BUILD_UNIX_TIME = 1743433997951L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
