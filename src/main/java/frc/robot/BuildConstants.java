bd38eb25059449b7199a653bae2c32af5fd8112b
ShaLin <mrsaltinecracker1@gmail.com>
2025-11-08 21:30:49 -0800

    MTTD End of Day

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 335;
-  public static final String GIT_SHA = "e8968bb344350a865c687858bc8b583f508fbb0e";
-  public static final String GIT_DATE = "2025-10-14 19:56:42 EDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-11-07 22:04:02 EST";
-  public static final long BUILD_UNIX_TIME = 1762571042485L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 336;
+  public static final String GIT_SHA = "8b4dae8fccd556cc242c9974f18d3bc818d45533";
+  public static final String GIT_DATE = "2025-11-07 22:05:15 EST";
+  public static final String GIT_BRANCH = "Madtown";
+  public static final String BUILD_DATE = "2025-11-08 13:53:29 EST";
+  public static final long BUILD_UNIX_TIME = 1762628009444L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
